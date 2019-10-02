82c43c8048492c0ea45d3913ceaafbbdf45adac4
Ganevru <johno-actions-push-subdirectories@example.org>
2019-10-02 18:02:04 +0000

    Update gatsby-starter-chronoblog from Ganevru/gatsby-theme-chronoblog

---
+++ README.md
@@ -0,0 +1,5 @@
+# Starter for Chronoblog Gatsby Theme
+
+gatsby-starter-chronoblog
+
+Idea of this starter not only help you to start a chronoblog website, but also teach how to use them.
---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: 'Chronoblog Starter',
+    siteDescription: 'Starter for Gatsby Theme Chronoblog',
+    siteImage: '/banner.png', // main image of the site for meta tags
+    siteUrl: 'http://localhost:8000', // example - http://example.com
+    pathPrefix: '/', // '/' - default, example - '/someprefix'
+    siteLanguage: 'en',
+    ogLanguage: `en_US`,
+    author: 'Site Author', // for example - 'Ivan Ganev'
+    twitter: '' // for twitter cards meta data, example - '@ganevru'
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-theme-chronoblog',
+      options: {
+        uiText: {
+          // ui text fot translate
+          feedShowMoreButton: '↓ show more ↓',
+          feedSearchPlaceholder: 'search',
+          cardReadMoreButton: 'read more →',
+          allTagsButton: 'all tags'
+        },
+        feedItemsLimit: 50
+      }
+    }
+  ]
+};
