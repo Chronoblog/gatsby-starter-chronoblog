f1db2c21ca07f5f66ef18509b5ba920041698aee
Ganevru <johno-actions-push-subdirectories@example.org>
2019-11-09 07:57:47 +0000

    Update gatsby-starter-chronoblog from Ganevru/gatsby-theme-chronoblog

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -3,23 +3,23 @@ import chronoblogTheme from 'gatsby-theme-chronoblog/src/gatsby-plugin-theme-ui'
 export default {
   ...chronoblogTheme,
   initialColorMode: 'light',
-  useColorSchemeMediaQuery: false,
   colors: {
-    ...chronoblogTheme.colors,
+    ...chronoblogTheme.color,
     text: '#000',
     background: '#fff',
+    link: '#3d7e9a',
     primary: '#3a5f7d',
     secondary: '#5a809e',
     muted: '#dae1e3',
     modes: {
       dark: {
-        ...chronoblogTheme.colors.modes.dark,
         text: '#fff',
         background: '#000',
         muted: '#161b1d'
       }
     }
   },
+  fontSizes: [14, 16, 18, 20, 22, 24, 28, 36],
   fonts: {
     ...chronoblogTheme.fonts,
     body: '-apple-system,BlinkMacSystemFont,Helvetica,Arial,sans-serif',
